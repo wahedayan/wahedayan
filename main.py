b9f2163032ee9a939291c61cdb0dbc3647cdcdeb
wahedayan <95471263+wahedayan@users.noreply.github.com>
2022-12-13 10:21:48 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,11 @@
+a=input("Enter the first number :")
+b=input("Enter the second number :")
+
+print("Addition of two numbers is :", int(a)+int(b))
+print("Subtraction of two numbers is :", int(a)-int(b))
+print("Multiplication of two numbers is :", int(a)*int(b))
+print("Division of two numbers is :", int(a)/int(b))
+print("Modulo of two numbers is :", int(a)%int(b))
+print("Floor Division of two numbers is :", int(a)//int(b))
+
+print("Exponential of two numbers is :", int(a)**int(b))
